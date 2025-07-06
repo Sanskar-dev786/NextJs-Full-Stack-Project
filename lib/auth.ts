f75f8826bd859b756a491b5b46b3afe73e2db8c4
Sanskar-dev786 <pandey1606sanskar@gmail.com>
2025-07-06 16:58:26 +0530

    authentication part releted to next auth is now almost comleted

---
+++ lib/auth.ts
@@ -0,0 +1,77 @@
+import { NextAuthOptions } from "next-auth";
+import CredentialsProvider from "next-auth/providers/credentials";
+import GithubProvider from "next-auth/providers/github";
+import GoogleProvider from "next-auth/providers/google";
+import { connectToDatabase } from "./db";
+import User from "@/models/User";
+import bcrypt from "bcryptjs";
+
+export const authOptions: NextAuthOptions = {
+  providers: [
+    CredentialsProvider({
+      name: "Credentials",
+      credentials: {
+        email: { label: "Email", type: "text" },
+        password: { label: "Password", type: "password" }
+      },
+      async authorize(credentials) {
+        if (!credentials?.email || !credentials?.password) {
+          throw new Error("Missing email or password")
+        }
+
+        try {
+          await connectToDatabase();
+          const user = await User.findOne({ email: credentials.email })
+          
+          if (!user) {
+            throw new Error("No user found with this");
+          }
+
+          const isValid = await bcrypt.compare(
+            credentials.password,
+            user.password
+          )
+
+          if (!isValid) {
+            throw new Error("invalid password");
+          }
+
+          return {
+            id: user.id.toString(),
+            email: user.email
+          }
+
+
+        } catch (error) {
+          console.error("Auth error", error)
+          throw error
+        }
+      }
+      
+    }),
+    GithubProvider({
+      clientId: process.env.GITHUB_ID!,
+      clientSecret: process.env.GITHUB_SECRET!,
+    }),
+    GoogleProvider({
+      clientId: process.env.GOOGLE_CLIENT_ID!,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET!
+    }),
+    
+  ],
+  callbacks: {
+    async jwt({ token, user }) {
+      if (user) {
+        token.id = user.id
+      }
+      return token
+    },
+    async session({ session, token, user }) {
+      if (session) {
+        session.user.id = token.id as string
+      }
+      return session
+    },
+
+  }
+};
